78b4b671a4e4e7f5a5072aedef5b6b1e3cb49009
nboober <nboober@terpmail.umd.edu>
2019-07-30 11:54:35 -0400

    Redirected the user to the login page once they register instead of the index page

--- src/main/java/com/example/demo/SecurityController.java
+++ src/main/java/com/example/demo/SecurityController.java
@@ -40,7 +40,7 @@ public class SecurityController {
             userService.saveUser(user);
             model.addAttribute("message", "User Account Created");
         }
-        return "index";
+        return "login";
 
     }
 
